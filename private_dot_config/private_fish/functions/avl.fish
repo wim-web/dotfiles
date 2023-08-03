18a668e390221be1597392c4863b562317035a14
wim-web <45655999+wim-web@users.noreply.github.com>
2023-08-03 19:52:40 +0900

    loginのsessionを12hにする

--- private_dot_config/private_fish/functions/avl.fish
+++ private_dot_config/private_fish/functions/avl.fish
@@ -1,6 +1,6 @@
 function avl
     set --local p (aws-vault list --profiles | peco)
     if string length -q -- $p
-        open -na "Google Chrome" --args --user-data-dir=$HOME/Library/Application\ Support/Google/Chrome/aws-vault/"$p" $(aws-vault login "$p" --stdout)
+        open -na "Google Chrome" --args --user-data-dir=$HOME/Library/Application\ Support/Google/Chrome/aws-vault/"$p" $(aws-vault login "$p" -d 12h --stdout)
     end
 end
